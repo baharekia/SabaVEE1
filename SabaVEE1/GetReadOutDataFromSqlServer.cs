46b9868f2b448ded428e8eb89701f062cad8b262
Bagherkia.Bahareh <Bagherkia.Bahareh@PC-ANSARI>
2019-12-02 15:50:30 +0330

    arranged

--- SabaVEE1/GetReadOutDataFromSqlServer.cs
+++ SabaVEE1/GetReadOutDataFromSqlServer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,24 @@ namespace SabaVEE1
 {
     public class GetReadOutDataFromSqlServer
     {
+        SqlConnection cnn;
+        string connectionString = null;
+        string sql = null;
         int i = 0;
         int j = 0;
         string data = null;
-
         List<object> ReadOutList = new List<object>();
 
-        public List<object> GetReadOutDataFromSqlServerMethod(DataSet ds)
+        public List<object> GetReadOutDataFromSqlServerMethod()
         {
+            connectionString = "Data Source=.;Initial Catalog=SabaCandH;User ID=sa;Password=88102351-7";
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            sql = "select distinct ReadDate,TransferDate,obis,Value,ObisFarsiDesc from Meter inner join OBISValueHeader on meter.MeterID = OBISValueHeader.MeterID inner join OBISValueDetail on OBISValueDetail.OBISValueHeaderID = OBISValueHeader.OBISValueHeaderID inner join OBISS on obiss.OBISID = OBISValueDetail.OBISID where Meter.MeterNumber = '1949400024728' and Value != '0'and(Obiss.OBISID = 83 or ObisFarsiDesc like '%آب مصرفي کل%' or Obiss.OBISID = 88 or ObisFarsiDesc like '%ساعت%') order by ReadDate,OBISS.Obis";
+            SqlDataAdapter dscmd = new SqlDataAdapter(sql, cnn);
+            DataSet ds = new DataSet();
+
+            dscmd.Fill(ds);
             for (i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
             {
                 object[] dataEntryArray = new object[6];
--- SabaVEE1/Program.cs
+++ SabaVEE1/Program.cs
@@ -17,40 +17,14 @@ namespace SabaVEE1
     {
         static void Main(string[] args)
         {
-            #region prerequisite
-            SqlConnection cnn;
-            string connectionString = null;
-            string sql = null;
-            
-            var shamsiDate = new Shamsi_to_Miladi_convertorDate();
-
-            Excel.Application xlApp;
-            Excel.Workbook xlWorkBook;
-            Excel.Worksheet xlWorkSheet;
-            object misValue = System.Reflection.Missing.Value;
-
-            xlApp = new Excel.Application();
-            xlWorkBook = xlApp.Workbooks.Add(misValue);
-            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-
-            connectionString = "Data Source=.;Initial Catalog=SabaCandH;User ID=sa;Password=88102351-7";
-
-            cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            sql = "select distinct ReadDate,TransferDate,obis,Value,ObisFarsiDesc from Meter inner join OBISValueHeader on meter.MeterID = OBISValueHeader.MeterID inner join OBISValueDetail on OBISValueDetail.OBISValueHeaderID = OBISValueHeader.OBISValueHeaderID inner join OBISS on obiss.OBISID = OBISValueDetail.OBISID where Meter.MeterNumber = '1949400024728' and Value != '0'and(Obiss.OBISID = 83 or ObisFarsiDesc like '%آب مصرفي کل%' or Obiss.OBISID = 88 or ObisFarsiDesc like '%ساعت%') order by ReadDate,OBISS.Obis";
-            
-            SqlDataAdapter dscmd = new SqlDataAdapter(sql, cnn);
-            DataSet ds = new DataSet();
-            dscmd.Fill(ds);
-
+            var shamsiDate = new Shamsi_to_Miladi_convertorDate(); 
             List<object> ReadOutList = new List<object>();
             List<AnalysisDataModel> FinalReadOutList = new List<AnalysisDataModel>();
             List<AnalysisDataModel> FinalOrderedReadOutList = new List<AnalysisDataModel>();
             List<AnalysisDataModel> ReadOutListNew = new List<AnalysisDataModel>();
-            #endregion
 
             GetReadOutDataFromSqlServer getReadOutDataFromSqlServer = new GetReadOutDataFromSqlServer();
-            ReadOutList = getReadOutDataFromSqlServer.GetReadOutDataFromSqlServerMethod(ds);
+            ReadOutList = getReadOutDataFromSqlServer.GetReadOutDataFromSqlServerMethod();
 
             CreateNewListWithDataModel createNewListWithDataModel = new CreateNewListWithDataModel();
             FinalReadOutList = createNewListWithDataModel.CreateNewListWithDataModelMethod(ReadOutList);
@@ -63,7 +37,6 @@ namespace SabaVEE1
 
             InsertToExcellFile InsertToExcellFile = new InsertToExcellFile();
             InsertToExcellFile.InsertToExcellFileMethod(ReadOutListNew, FinalOrderedReadOutList);
-            
         }
     }
 }
