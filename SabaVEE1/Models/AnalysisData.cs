7dd63be615f0758757835b241ca834a2542b6f7d
Bagherkia.Bahareh <Bagherkia.Bahareh@PC-ANSARI>
2019-12-07 08:11:16 +0330

    ,mkl

---
+++ SabaVEE1/Models/AnalysisData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabaVEE1.Models
+{
+    public class AnalysisData
+    {
+        public DateTime ReadOutDate = new DateTime();
+        public string TransferDate = null;
+        public string Obis = null;
+        public string Value = null;
+        public string ObisFarciDesc = null;
+        public string Date = "";
+    }
+}
---
+++ SabaVEE1/ReadOutDataAnalysisContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabaVEE1
+{
+    public class ReadOutDataAnalysisContext : DbContext
+    {
+        public ReadOutDataAnalysisContext() : base()
+        {
+
+        }
+
+        public DbSet<AnalysisDataModel> Students { get; set; }
+    }
+}
